1e9d795100a68e712b43921ea8b8ca82ef5a5374
theblurrysean <79564574+GangeiKan510@users.noreply.github.com>
2024-06-20 22:06:57 +0800

    chore: setup default palette on tailwind and installed firebase dependency

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,28 +1,10 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-
-import { Colors } from '../../constants/Colors';
-import { useColorScheme } from '../../hooks/useColorScheme';
-import { Text } from 'react-native';
+import { Tabs } from "expo-router";
+import React from "react";
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: '',
-          tabBarIcon: ({ color, focused }) => (
-            <Text></Text>
-          ),
-        }}
-      />
+    <Tabs>
+      <Tabs.Screen name="index" options={{ headerShown: false}}/>
     </Tabs>
   );
 }
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,35 +1,72 @@
-import { View, Text } from "react-native";
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import useCustomFonts from "@/hooks/useCustomFonts";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback } from "react";
 
-export default function HomeScreen() {
-  const fontsLoaded = useCustomFonts();
+const HomeScreen = () => {
+  const [fontsLoaded, fontError] = useFonts({
+    "Inter-Regular": require("../../assets/fonts/Inter-Regular.ttf"),
+    "Judson-Bold": require("../../assets/fonts/Judson-Bold.ttf"),
+  });
 
-  if (!fontsLoaded) {
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
     return null;
   }
 
   return (
-    <SafeAreaView>
-      <View>
+    <SafeAreaView className="flex-1 bg-white" onLayout={onLayoutRootView}>
+      <View className="mt-5">
         <View>
-          <Text className="text-center text-[48px]" style={styles.font.logo}>
-            StyleSync
+          <Text className="text-center text-[48px] font-logo">StyleSync</Text>
+          <Text className="text-center text-[16px] font-sans">
+            Style Effortlessly.
           </Text>
-          <Text className="text-center text-[16px]" style={styles.font.normal}>Style Effortlessly.</Text>
+        </View>
+        <View className="mx-8 mt-10">
+          <Text className="font-bold text-[20px] font-sans">
+            Thank you for styling with us!
+          </Text>
+          <Text className="text-base font-sans">
+            Please enter your details for registration:
+          </Text>
+          <View className="mt-10">
+            <Text className="text-base font-sans">First Name</Text>
+            <TextInput className="bg-bg-secondary text-base rounded-[10px] h-[42px] px-3 mt-1 mb-3" />
+            <Text className="text-base font-sans">Last Name</Text>
+            <TextInput className="bg-bg-secondary text-base rounded-[10px] h-[42px] px-3 mt-1 mb-3" />
+            <Text className="text-base font-sans">Email</Text>
+            <TextInput className="bg-bg-secondary text-base rounded-[10px] h-[42px] px-3 mt-1 mb-3" />
+            <Text className="text-base font-sans">Password</Text>
+            <TextInput className="bg-bg-secondary text-base rounded-[10px] h-[42px] px-3 mt-1 mb-3" />
+            <Text className="text-base font-sans">Confirm Password</Text>
+            <TextInput className="bg-bg-secondary text-base rounded-[10px] h-[42px] px-3 mt-1 mb-3" />
+            <TouchableOpacity className="h-[42px] bg-bg-tertiary rounded-[10px] justify-center my-5">
+              <Text className="text-center text-white text-base align-middle">
+                Sign Up
+              </Text>
+            </TouchableOpacity>
+            <View className="flex-row items-center my-5">
+              <View className="flex-1 h-px bg-[#b7b7b7]" />
+              <Text className="mx-4 text-base">or</Text>
+              <View className="flex-1 h-px bg-[#b7b7b7]" />
+            </View>
+            <TouchableOpacity className="h-[42px] bg-bg-secondary rounded-[10px] justify-center my-5">
+              <Text className="text-center text-base align-middle">
+                Sign up with Google
+              </Text>
+            </TouchableOpacity>
+          </View>
         </View>
       </View>
     </SafeAreaView>
   );
-}
-
-const styles = {
-  font: {
-    logo: {
-      fontFamily: "Judson-Bold",
-    },
-    normal: {
-      fontFamily: "Inter-Regular"
-    }
-  },
 };
+
+export default HomeScreen;
