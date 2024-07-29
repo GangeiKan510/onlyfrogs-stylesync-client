7d188d48385cb69601773f1a1e6cf7c204db1aa0
theblurrysean <79564574+GangeiKan510@users.noreply.github.com>
2024-07-29 23:13:12 +0800

    feat: add validation to registration phase

--- app/register.tsx
+++ app/register.tsx
@@ -1,4 +1,4 @@
-import { TextInput, View, Text, Pressable, KeyboardAvoidingView } from 'react-native';
+import { TextInput, View, Text, Pressable, KeyboardAvoidingView, Alert } from 'react-native';
 import { useState } from 'react';
 import Header from '@/components/common/Header';
 import { Link, useRouter } from 'expo-router';
@@ -6,19 +6,28 @@ import { signUp } from '@/components/auth/sign-up';
 import { createUser } from '@/network/web/user';
 import { routes } from '@/utils/routes';
 import CustomButton from '@/components/buttons/CustomButton';
+import { validateForm } from '@/components/validators/verifyRegistrationDetails';
 
 export default function Register() {
-
   const router = useRouter();
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [loading, setLoading] = useState(false);
 
-  const [ firstName, setFirstName ] = useState('');
-  const [ lastName, setLastName ] = useState('');
-  const [ email, setEmail ] = useState('');
-  const [ password, setPassword ] = useState('');
-  const [ loading, setLoading ] = useState(false);
-
-  const handleSignUp = async() => {
+  const handleSignUp = async () => {
     setLoading(true);
+
+    const { success, message } = validateForm(firstName, lastName, email, password, confirmPassword);
+    
+    if (!success) {
+      Alert.alert('Registration Error', message);
+      setLoading(false);
+      return;
+    }
+
     try {
       const userData = {
         first_name: firstName,
@@ -26,22 +35,19 @@ export default function Register() {
         email: email,
       };
 
-      console.log(userData)
-
       const firebaseAuthUser = await signUp(email, password);
       const newUser = await createUser(userData);
-      console.log(firebaseAuthUser);
-      console.log(newUser);
       setLoading(false);
-      if (firebaseAuthUser) {
+      
+      if (firebaseAuthUser && newUser) {
         router.replace(routes.login);
       }
     } catch (error) {
-      console.log(error);
-      alert('Sign in failed:' + error);
+      console.error(error);
+      Alert.alert('Registration Error', 'Please check your credentials and try again.');
       setLoading(false);
     }
-  }
+  };
 
   return (
     <View className='flex-1 bg-[#ffffff]'>
@@ -55,27 +61,51 @@ export default function Register() {
           <View>
             <View className='mb-3'>
               <Text className='mb-1'>First Name<Text className='text-[#EE4E4E]'> *</Text></Text>
-              <TextInput className='bg-[#F3F3F3] h-[42px] rounded-[10px] px-4' value={firstName} onChangeText={(input) => setFirstName(input)}/>
+              <TextInput
+                className='bg-[#F3F3F3] h-[42px] rounded-[10px] px-4'
+                value={firstName}
+                onChangeText={setFirstName}
+              />
             </View>
             <View className='mb-3'>
               <Text className='mb-1'>Last Name<Text className='text-[#EE4E4E]'> *</Text></Text>
-              <TextInput className='bg-[#F3F3F3] h-[42px] rounded-[10px] px-4' value={lastName} onChangeText={(input) => setLastName(input)}/>
+              <TextInput
+                className='bg-[#F3F3F3] h-[42px] rounded-[10px] px-4'
+                value={lastName}
+                onChangeText={setLastName}
+              />
             </View>
             <View className='mb-3'>
               <Text className='mb-1'>Email<Text className='text-[#EE4E4E]'> *</Text></Text>
-              <TextInput className='bg-[#F3F3F3] h-[42px] rounded-[10px] px-4' value={email} onChangeText={(input) => setEmail(input)}/>
+              <TextInput
+                className='bg-[#F3F3F3] h-[42px] rounded-[10px] px-4'
+                value={email}
+                onChangeText={setEmail}
+              />
             </View>
             <View className='mb-3'>
               <Text>Password<Text className='text-[#EE4E4E]'> *</Text></Text>
-              <TextInput className='bg-[#F3F3F3] h-[42px] rounded-[10px] px-4' value={password} onChangeText={(input) => setPassword(input)} secureTextEntry={true}/>
+              <TextInput
+                className='bg-[#F3F3F3] h-[42px] rounded-[10px] px-4'
+                value={password}
+                onChangeText={setPassword}
+                secureTextEntry={true}
+              />
             </View>
             <View className='mb-8'>
               <Text className='mb-1'>Confirm Password<Text className='text-[#EE4E4E]'> *</Text></Text>
-              <TextInput className='bg-[#F3F3F3] h-[42px] rounded-[10px] px-4'/>
+              <TextInput
+                className='bg-[#F3F3F3] h-[42px] rounded-[10px] px-4'
+                value={confirmPassword}
+                onChangeText={setConfirmPassword}
+                secureTextEntry={true}
+              />
             </View>
             <View>
               <CustomButton isLoading={loading} callBack={handleSignUp} label={'Sign up'} type={'primary'} /> 
-              <Link href={'/login'} className='text-[#7ab2b2] underline text-center mt-3'>I already have an account</Link>
+              <Link href={routes.login} className='text-[#7ab2b2] underline text-center mt-3'>
+                I already have an account
+              </Link>
               <View className='flex-row items-center my-4'>
                 <View className='flex-1 h-px bg-gray-400'></View>
                 <Text className='text-center mx-4'>or</Text>
